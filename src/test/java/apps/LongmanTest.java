a563f61dd7f901fb849dcf332ab4f961c325a7ec
Anita Cruz <ntcrz@hotmail.com>
2025-08-29 12:44:21 -0400

    Update to include Longman and BaseTest

---
+++ src/test/java/apps/LongmanTest.java
@@ -0,0 +1,29 @@
+package apps;
+
+import common.BaseTest;
+import io.appium.java_client.AppiumBy;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LongmanTest extends BaseTest
+{
+    @BeforeAll
+    static void appOptions()
+    {
+        options.setCapability("appPackage", "advanced3nd.ofamerican.english");
+        options.setCapability("appActivity",".view.SplashActivity");
+        options.setCapability("appWaitActivity","view.MainTabActivity");
+    }
+
+    @Test
+    void longman()
+    {
+
+        driver.findElement(AppiumBy.id("advanced3nd.ofamerican.english:id/etValueSearch")).sendKeys("selenium");
+        WebElement result = driver.findElement(AppiumBy.id("advanced3nd.ofamerican.english:id/tvWord"));
+        assertEquals("selenium", result.getText());
+    }
+
+}
---
+++ src/test/java/common/BaseTest.java
@@ -0,0 +1,39 @@
+package common;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.remote.MobilePlatform;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BaseTest
+{
+    protected AndroidDriver driver;
+    static protected UiAutomator2Options options;
+
+    @BeforeAll
+    static void commonOptions()
+    {
+        options = new UiAutomator2Options();
+        options.setPlatformName(MobilePlatform.ANDROID);
+    }
+
+    @BeforeEach
+    void driverSetup() throws MalformedURLException
+    {
+        driver = new AndroidDriver(new URL("http://192.168.4.44:4723"), options);
+    }
+
+
+    @AfterEach
+    void shutDriver()
+    {
+      driver.quit();
+
+    }
+
+
+}
